dc097dd4df9a69468d544e250dcbfad572c0a2cd
fkarbing <fkarbing@gmail.com>
2019-12-11 21:01:40 +0100

    configure cognito

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'eu-west-1_GQwgozfTB', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '2353elk0npej715o1jmr0sppq0', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-west-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
